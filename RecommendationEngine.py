e9c89fa481ed6c53d2d2edd99ec8237d23c19707
sakethkaparthi <sakethkaparthi@outlook.com>
2016-10-14 02:27:17 +0530

    Added API integrationa and some styling for popular images

--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -2,11 +2,28 @@ from flask import Flask
 from flask import render_template
 import sqlite3
 from databaseMethods import *
+import requests
 
 app = Flask(__name__)
 
 conn = sqlite3.connect('recommendation_engine.db')
-popular = getPopularMovies(conn, 20)
+p = getPopularMovies(conn, 10)
+ids = []
+popular = []
+param = {'api_key': "d4dc650ab1e0ee61a3a4e17453f770f5"}
+info_link = "https://api.themoviedb.org/3/movie/"
+poster_path = "https://image.tmdb.org/t/p/w185"
+
+for i in range(len(p)):
+    m = list(p[i])
+    id = getTmdbId(conn, m[0])
+    id = int(float((str(id[0])[1:-2])))
+    r = requests.get(info_link + str(id), params=param)
+    data = r.json()
+    movie = {"name": data['original_title'], "overview": data['overview'],
+             "poster": str(poster_path + data['poster_path'])}
+    print movie
+    popular.append(movie)
 
 
 @app.route('/')
--- databaseMethods.py
+++ databaseMethods.py
@@ -1,14 +1,23 @@
-def getPopularMovies(conn,limit):
-	return conn.execute("""select movies.movieId,title,avg(rating) as rate,count(rating) as cnt from movies
+def getPopularMovies(conn, limit):
+    return conn.execute("""select movies.movieId,title,avg(rating) as rate,count(rating) as cnt from movies
 	inner join ratings on movies.movieId = ratings.movieId 
     group by movies.movieId
     order by cnt desc 
-    limit """+str(limit)+";").fetchall()
+    limit """ + str(limit) + ";").fetchall()
 
-def getHighestRatedMovies(conn,mini,limit):
-	return conn.execute("""select movies.movieId,title,avg(rating) as rate,count(rating) as cnt from movies
+
+def getHighestRatedMovies(conn, mini, limit):
+    return conn.execute("""select movies.movieId,title,avg(rating) as rate,count(rating) as cnt from movies
 	inner join ratings on movies.movieId = ratings.movieId
 	group by movies.movieId
 	having cnt > """ + str(mini) + """ 
 	order by rate desc 
-	limit """+str(limit)+";").fetchall()
+	limit """ + str(limit) + ";").fetchall()
+
+
+def getTmdbId(conn, id):
+    return conn.execute("select tmdbId from links where movieId=" + str(id)).fetchall()
+
+
+def getTmdbIds(conn, id):
+    return conn.execute("select tmdbId from links")
