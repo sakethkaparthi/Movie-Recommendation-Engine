921b34ad00d638e94590554218d38ce70b8c8b0f
sakethkaparthi <sakethkaparthi@outlook.com>
2016-10-18 15:26:50 +0530

    MongoDB for imdb data of movies

--- RecommendationEngine.py
+++ RecommendationEngine.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask import render_template
-import sqlite3
 from databaseMethods import *
+from pymongo import MongoClient
+import sqlite3
 import requests
 
 app = Flask(__name__)
-
+client = MongoClient('localhost', 27017)
 conn = sqlite3.connect('recommendation_engine.db')
-p = getPopularMovies(conn, 5)
+db = client['movies-db']
+movies = db["movies"]
+
+p = getPopularMovies(conn, 50)
 ids = []
 popular = []
 param = {'api_key': "d4dc650ab1e0ee61a3a4e17453f770f5"}
@@ -19,11 +23,14 @@ for i in range(len(p)):
     m = list(p[i])
     id = getTmdbId(conn, m[0])
     id = int(float((str(id[0])[1:-2])))
-    r = requests.get(info_link + str(id), params=param)
-    data = r.json()
-    movie = {"name": data['original_title'], "overview": data['overview'],
-             "poster": str(poster_path + data['poster_path']),
-             "imdb": imdb_link.format(id=data["imdb_id"])}
+    movie = movies.find_one({"id": id})
+    if not movie:
+        r = requests.get(info_link + str(id), params=param)
+        data = r.json()
+        movie = {"id": id, "name": data['original_title'], "overview": data['overview'],
+                 "poster": str(poster_path + data['poster_path']),
+                 "imdb": imdb_link.format(id=data["imdb_id"])}
+        movies.insert_one(movie)
     print movie
     popular.append(movie)
 
@@ -35,7 +42,12 @@ def home():
 
 @app.route('/popular_movies')
 def popularmovies():
-    return render_template('popularmovies.html', movies=popular)
+    return render_template('movies.html', movies=popular)
+
+
+@app.route('/highest_rated')
+def highestratedmovies():
+    return render_template('movies.html')
 
 
 if __name__ == '__main__':
--- computations.py
+++ computations.py
@@ -1,5 +1,6 @@
 from math import sqrt
 
+
 def pearsonCorrelation(person1, person2, ratingsDict):
     both_rated = {}
     for item in ratingsDict[person1]:
@@ -12,55 +13,58 @@ def pearsonCorrelation(person1, person2, ratingsDict):
     person1_preferences_sum = sum([ratingsDict[person1][item] for item in both_rated])
     person2_preferences_sum = sum([ratingsDict[person2][item] for item in both_rated])
 
-    person1_square_preferences_sum = sum([pow(ratingsDict[person1][item],2) for item in both_rated])
-    person2_square_preferences_sum = sum([pow(ratingsDict[person2][item],2) for item in both_rated])
+    person1_square_preferences_sum = sum([pow(ratingsDict[person1][item], 2) for item in both_rated])
+    person2_square_preferences_sum = sum([pow(ratingsDict[person2][item], 2) for item in both_rated])
 
     product_sum_of_both_users = sum([ratingsDict[person1][item] * ratingsDict[person2][item] for item in both_rated])
 
-    num_value = product_sum_of_both_users - (person1_preferences_sum * person2_preferences_sum)/len(both_rated)
-    den_value = sqrt((person1_square_preferences_sum - pow(person1_preferences_sum,2)/len(both_rated)) *
-    (person2_square_preferences_sum - pow(person2_preferences_sum,2)/len(both_rated)))
-
+    num_value = product_sum_of_both_users - (person1_preferences_sum * person2_preferences_sum) / len(both_rated)
+    try:
+        den_value = sqrt((person1_square_preferences_sum - pow(person1_preferences_sum, 2) / len(both_rated)) *
+                         (person2_square_preferences_sum - pow(person2_preferences_sum, 2) / len(both_rated)))
+    except:
+        print "Exception"
+        print person1_preferences_sum, person1_square_preferences_sum, person2_preferences_sum, person2_square_preferences_sum
+        pass
     if den_value == 0:
         return 0
     else:
-        score = num_value/den_value
+        score = num_value / den_value
         return score
 
 
 def similarUsers(person, userIds, ratingsDict):
     similarUserDict = {}
     for userId in userIds:
-        score = pearsonCorrelation(person,userId, ratingsDict)
+        score = pearsonCorrelation(person, userId, ratingsDict)
         if score > 0:
             similarUserDict[str(userId)] = float("{0:.4f}".format(score))
 
-    similarUserDict = sorted(similarUserDict.iteritems(), key=lambda (k,v): (v,k), reverse=True)
+    similarUserDict = sorted(similarUserDict.iteritems(), key=lambda (k, v): (v, k), reverse=True)
     return similarUserDict
 
 
 def userRecommendations(person, ratingsDict):
-	totals = {}
-	simSums = {}
-	rankings_list = []
+    totals = {}
+    simSums = {}
+    rankings_list = []
 
-	for other in ratingsDict:
-		if other==person:
-			continue
-		sim = pearsonCorrelation(person,other, ratingsDict)
+    for other in ratingsDict:
+        if other == person:
+            continue
+        sim = pearsonCorrelation(person, other, ratingsDict)
 
-		if sim <= 0.5:
-			continue
-		for item in ratingsDict[other]:
-			if item not in ratingsDict[person] or ratingsDict[person][item] == 0:
+        if sim <= 0.5:
+            continue
+        for item in ratingsDict[other]:
+            if item not in ratingsDict[person] or ratingsDict[person][item] == 0:
+                totals.setdefault(item, 0)
+                totals[item] += ratingsDict[other][item] * sim
+                simSums.setdefault(item, 0)
+                simSums[item] += sim
 
-				totals.setdefault(item,0)
-				totals[item] += ratingsDict[other][item] * sim
-				simSums.setdefault(item,0)
-				simSums[item] += sim
-
-	rankings = [(total/simSums[item],item) for item,total in totals.items()]
-	rankings.sort()
-	rankings.reverse()
-	recommendations_list = [(recommendations_item,score) for score, recommendations_item in rankings]
-	return recommendations_list
+    rankings = [(total / simSums[item], item) for item, total in totals.items()]
+    rankings.sort()
+    rankings.reverse()
+    recommendations_list = [(recommendations_item, score) for score, recommendations_item in rankings]
+    return recommendations_list
